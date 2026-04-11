83d64e164e6cbb40f9e8f7f85edb88a5f2e9ba53
Dennis Claassen <declaassen@gmail.com>
2026-04-11 09:01:45 +0200

    Create Blog.php

---
+++ lib/Generators/Blog.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace DC23\ExcessiveSchema\Generators;
+
+use \Yoast\WP\SEO\Generators\Schema\Abstract_Schema_Piece;
+
+/**
+ * Returns blog schema markup.
+ */
+class Blog extends Abstract_Schema_Piece {
+
+	/**
+	 * Determines whether a piece should be added to the graph.
+	 *
+	 * @return bool
+	 */
+	public function is_needed() {
+        if ( is_term() ) {
+            return true;
+        }
+
+        return false;
+	}
+
+	/**
+	 * Generate pieces for thr graph.
+	 *
+	 * @return array<list<string, mixed>>
+	 */
+	public function generate() {
+		$graph = [];
+
+		$graph[] = $this->generate_blog();
+
+		return $graph;
+	}
+
+	/**
+	 * Generate a Blog piece.
+	 *
+	 *
+	 * @return array<sting, mixed>
+	 */
+        protected function generate_blog(): array {
+                $blog_id = $this->context->indexable->object_id;
+                $id      = $this->context->site_url . '#/schema/blog/' . \esc_attr( $blog_id );
+
+
+                $data = [
+                        '@type' => 'Blog,
+                        '@id'   => $id,
+                ];
+
+                return $data;
+        }
+}
