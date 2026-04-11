2aa24609b083a081d469a2948fd9da75f7079256
Dennis Claassen <declaassen@gmail.com>
2026-04-11 08:23:56 +0200

    Update Blog_Schema_Test.php

--- tests/Blog_Schema_Test.php
+++ tests/Blog_Schema_Test.php
@@ -65,6 +65,134 @@ class Blog_Schema_Test extends \WP_UnitTestCase {
 		$this->assertSame('News', $blog_piece['name']);
 	}
 
+	/**
+	 * @testdox Should not contain Blog piece when the article type is not BlogPosting
+	 */
+	public function test_should_not_add_blog_piece_when_article_type_is_not_blog_posting(): void {
+		$post_id = self::factory()->post->create(
+			array(
+				'title'        => 'unchanged',
+				'post_content' => 'Hello world!',
+			)
+		);
+
+		\YoastSEO()->helpers->options->set( 'schema-article-type-post', '' );
+		\YoastSEO()->helpers->meta->delete( 'schema_article_type', $post_id );
+
+		$this->go_to( \get_permalink( $post_id ) );
+
+		$schema_output = $this->get_schema_output();
+
+		$this->assertJson( $schema_output );
+
+		$schema_data = json_decode( $schema_output, JSON_OBJECT_AS_ARRAY );
+
+		$article_piece = $this->get_piece_by_type( $schema_data['@graph'], 'Article' );
+
+		$this->assertSame('Article', $article_piece['@type'],'Article piece should just be Article');
+	}
+
+	/**
+	 * @testdox Should contain Blog piece when the article type for the post type is BlogPosting
+	 */
+	public function test_should_add_blog_piece_when_default_article_type_is_blog_posting(): void {
+		$post_id = self::factory()->post->create(
+			array(
+				'title'        => 'default setting',
+				'post_content' => 'Hello world!',
+				'post_type'    => 'post',
+			)
+		);
+
+		// Set the default Schema.org article type for the post type "post" to BlogPosting.
+		\YoastSEO()->helpers->options->set( 'schema-article-type-post', 'BlogPosting' );
+
+		$this->go_to( \get_permalink( $post_id ) );
+
+		$schema_output = $this->get_schema_output();
+
+		$this->assertJson( $schema_output );
+
+		$schema_data = json_decode( $schema_output, JSON_OBJECT_AS_ARRAY );
+
+		$article_piece = $this->get_piece_by_type( $schema_data['@graph'], [ 'Article', 'BlogPosting' ] );
+		$blog_piece = $this->get_piece_by_type( $schema_data['@graph'], 'Blog' );
+
+		$this->assertSame(['Article','BlogPosting'], $article_piece['@type'],'Article graph piece should be BlogPosting');
+		$this->assertSame('Blog', $blog_piece['@type'],'Blog graph piece should exist');
+		$this->assertSame($article_piece['@id'], $blog_piece['blogPost'][0]['@id'],'Blog should refer to BlogPosting');
+	}
+
+	/**
+	 * @testdox Should contain Blog piece when the article type for the single post is BlogPosting
+	 */
+	public function test_should_contain_blog_piece_when_article_type_for_post_is_blog_posting(): void {
+		$post_id = self::factory()->post->create(
+			array(
+				'title'        => 'indexable setting',
+				'post_content' => 'Hello world!',
+			)
+		);
+
+		\YoastSEO()->helpers->meta->set_value( 'schema_article_type', 'BlogPosting', $post_id );
+
+		// Update object to persist meta value to indexable.
+		self::factory()->post->update_object( $post_id, [] );
+
+		$this->go_to( \get_permalink( $post_id ) );
+
+		$schema_output = $this->get_schema_output();
+
+		$this->assertJson( $schema_output );
+
+		$schema_data = \json_decode( $schema_output, JSON_OBJECT_AS_ARRAY );
+
+		$article_piece = $this->get_piece_by_type( $schema_data['@graph'], [ 'Article', 'BlogPosting' ] );
+		$blog_piece = $this->get_piece_by_type( $schema_data['@graph'], 'Blog' );
+
+		$this->assertSame(['Article','BlogPosting'], $article_piece['@type'],'Article piece should be BlogPosting');
+		$this->assertSame('Blog', $blog_piece['@type'],'Blog graph piece should exit');
+		$this->assertSame($article_piece['@id'], $blog_piece['blogPost'][0]['@id'],'Blog should refer to BlogPosting');
+	}
+
+	/**
+	 * @testdox Should not contain Blog piece when the post has multiple categories
+	 */
+	public function test_should_not_contain_blog_piece_when_post_has_multiple_categories(): void {
+		$post_id = self::factory()->post->create(
+			array(
+				'title'        => 'indexable setting',
+				'post_content' => 'Hello world!',
+			)
+		);
+
+		\YoastSEO()->helpers->meta->set_value( 'schema_article_type', 'BlogPosting', $post_id );
+
+		\wp_set_post_categories(
+			$post_id,
+			[
+				self::factory()->category->create(),
+				self::factory()->category->create(),
+				self::factory()->category->create(),
+			]
+		);
+
+		// Update object to persist meta value to indexable.
+		self::factory()->post->update_object( $post_id, [] );
+
+		$this->go_to( \get_permalink( $post_id ) );
+
+		$schema_output = $this->get_schema_output();
+
+		$this->assertJson( $schema_output );
+
+		$schema_data = \json_decode( $schema_output, JSON_OBJECT_AS_ARRAY );
+
+		$article_piece = $this->get_piece_by_type( $schema_data['@graph'], [ 'Article', 'BlogPosting' ] );
+
+		$this->assertSame([ 'Article', 'BlogPosting' ], $article_piece['@type'],'Article graph piece should be BlogPosting');
+	}
+
 	private function get_schema_output( bool $debug_wpseo_head = false ): string {
 
 		ob_start();
