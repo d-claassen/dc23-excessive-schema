53ce07dd5bc037099e9355280e9780f767f35f1b
Dennis Claassen <dennis@yoast.com>
2026-04-14 17:34:23 +0200

    Create Blog schema markup (#8)

---
+++ lib/Generators/Blog.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace DC23\ExcessiveSchema\Generators;
+
+use \WP_Post;
+use \WP_Term;
+use \Yoast\WP\SEO\Generators\Schema\Abstract_Schema_Piece;
+
+/**
+ * Returns blog schema markup.
+ */
+class Blog extends Abstract_Schema_Piece {
+
+	/**
+	 * Determines whether a piece should be added to the graph.
+	 *
+	 * @return bool
+	 */
+	public function is_needed() {
+		if ( is_category() ) {
+			return true;
+		}
+
+		if ( \is_single() && \get_post_type() === 'post' ) {
+			$schema_article_type = (array) $this->context->schema_article_type;
+
+			// Only needed in case of BlogPosting.
+			if ( ! \in_array( 'BlogPosting', $schema_article_type, true ) ) {
+				return false;
+			}
+			
+			// Only needed if the post has exactly 1 category.
+			// @TODO. consider blog pieces for each category.
+			$categories = \wp_get_post_categories( $this->context->indexable->object_id, [ 'fields' => 'all' ] );
+			if ( \count( $categories ) !== 1 ) {
+				return false;
+			}
+			
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Generate pieces for the graph.
+	 *
+	 * @return list<array<string, mixed>>
+	 */
+	public function generate() {
+		$graph = [];
+
+		$graph[] = $this->generate_blog();
+
+		return $graph;
+	}
+
+	/**
+	 * Generate a Blog piece.
+	 *
+	 *
+	 * @return array<sting, mixed>
+	 */
+	protected function generate_blog(): array {
+		$blog_id = $this->context->indexable->object_id;
+		$id      = $this->context->site_url . '#/schema/blog/' . \esc_attr( $blog_id );
+
+		if ( \is_category() ) {
+			$category = \get_term( \get_query_var( 'cat' ), 'category' );
+		} else{
+			$post = \get_post( \get_the_ID() );
+			\assert( $post instanceof WP_Post );
+
+			$categories = \wp_get_post_categories( $post->ID, [ 'fields' => 'all' ] );
+			if ( \count( $categories ) !== 1 ) {
+				// Only add Blog piece when there's one category:
+				// - Without category, there's no blog to connect with,
+				// - With multiple categories, it'll be a PITA to make sense.
+				return [];
+			}
+
+			$category = \reset( $categories );
+		}
+		\assert( $category instanceof WP_Term );
+
+		$data = [
+				'@id'         => $id,
+				'@type'       => 'Blog',
+				'name'        => $category->name,
+				'description' => \wp_trim_excerpt( $category->description ),
+				'inLanguage'  => \get_bloginfo( 'language' ),
+			];
+			
+			if ( $this->context->site_represents !== false ) {
+				$data['publisher'] = $this->context->site_represents_reference;
+			}
+			
+			if ( isset( $post ) ) {
+				$id      = \get_permalink( $post->ID ) . '#article';
+				$post_id = [ '@id' => $id ];
+
+				$data[	'blogPost' ] = [ $post_id ];
+			}
+		
+		return $data;
+	}
+}
---
+++ tests/Blog_Schema_Test.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace DC23\ExcessiveSchema\Tests;
+
+/**
+ * Class Blog_Schema_Test.
+ *
+ * @testdox Blog schema markup for a category
+ */
+class Blog_Schema_Test extends \WP_UnitTestCase {
+
+	private $user_id;
+
+	public function setUp(): void {
+		parent::setUp();
+
+		// Yoast user settings
+		$this->user_id = self::factory()->user->create();
+
+		\YoastSEO()->helpers->options->set( 'company_or_person', 'person' );
+		\YoastSEO()->helpers->options->set( 'company_or_person_user_id', $this->user_id );
+	}
+
+	// override wordpress function thats incompatible
+	// with phpunit 10.
+	public function expectDeprecated(){}
+
+	public function test_should_contain_blog_piece_as_main_entity(): void {
+		$post_id = self::factory()->post->create( [
+			'title'        => 'New post',
+			'post_content' => 'Hello world!',
+		] );
+
+		$category_id = self::factory()->category->create( [
+				'name' => 'News',
+		] );
+
+		\wp_set_post_categories( $post_id, [ $category_id ] );
+
+		$this->go_to( \get_category_link( $category_id ) );
+
+		$schema_output = $this->get_schema_output( true );
+		$schema_data = \json_decode( $schema_output, JSON_OBJECT_AS_ARRAY );
+
+		$blog_piece    = $this->get_piece_by_type( $schema_data['@graph'], 'Blog' );
+		$webpage_piece = $this->get_piece_by_type( $schema_data['@graph'], 'CollectionPage' );
+		$org_piece     = $this->get_piece_by_type( $schema_data['@graph'], ['Person', 'Organization'] );
+
+		$this->assertSame(
+			$blog_piece['@id'],
+			$webpage_piece['mainEntity']['@id'],
+			'MainEntity should be Blog'
+		);
+		$this->assertSame(
+			'http://localhost:8889/#/schema/blog/2',
+			$blog_piece['@id'],
+			'Format {{website}}/#/schema/{{type}}/{{ID}}'
+		);
+		$this->assertSame('News', $blog_piece['name']);
+		$this->assertSame(
+			$org_piece['@id'],
+			$blog_piece['publisher']['@id'],
+			'Blog publisher should be org'
+		);
+	}
+
+	/**
+	 * @testdox Should not contain Blog piece when the article type is not BlogPosting
+	 */
+	public function test_should_not_add_blog_piece_when_article_type_is_not_blog_posting(): void {
+		$post_id = self::factory()->post->create(
+			array(
+				'title'        => 'unchanged',
+				'post_content' => 'Hello world!',
+			)
+		);
+
+		\YoastSEO()->helpers->options->set( 'schema-article-type-post', '' );
+		\YoastSEO()->helpers->meta->delete( 'schema_article_type', $post_id );
+
+		$this->go_to( \get_permalink( $post_id ) );
+
+		$schema_output = $this->get_schema_output();
+
+		$this->assertJson( $schema_output );
+
+		$schema_data = json_decode( $schema_output, JSON_OBJECT_AS_ARRAY );
+
+		$article_piece = $this->get_piece_by_type( $schema_data['@graph'], 'Article' );
+
+		$this->assertSame('Article', $article_piece['@type'],'Article piece should just be Article');
+	}
+
+	/**
+	 * @testdox Should contain Blog piece when the article type for the post type is BlogPosting
+	 */
+	public function test_should_add_blog_piece_when_default_article_type_is_blog_posting(): void {
+		$post_id = self::factory()->post->create(
+			array(
+				'title'        => 'default setting',
+				'post_content' => 'Hello world!',
+				'post_type'    => 'post',
+			)
+		);
+
+		// Set the default Schema.org article type for the post type "post" to BlogPosting.
+		\YoastSEO()->helpers->options->set( 'schema-article-type-post', 'BlogPosting' );
+
+		$this->go_to( \get_permalink( $post_id ) );
+
+		$schema_output = $this->get_schema_output();
+
+		$this->assertJson( $schema_output );
+
+		$schema_data = json_decode( $schema_output, JSON_OBJECT_AS_ARRAY );
+
+		$article_piece = $this->get_piece_by_type( $schema_data['@graph'], [ 'Article', 'BlogPosting' ] );
+		$blog_piece = $this->get_piece_by_type( $schema_data['@graph'], 'Blog' );
+
+		$this->assertSame(['Article','BlogPosting'], $article_piece['@type'],'Article graph piece should be BlogPosting');
+		$this->assertSame('Blog', $blog_piece['@type'],'Blog graph piece should exist');
+		$this->assertSame($article_piece['@id'], $blog_piece['blogPost'][0]['@id'],'Blog should refer to BlogPosting');
+	}
+
+	/**
+	 * @testdox Should contain Blog piece when the article type for the single post is BlogPosting
+	 */
+	public function test_should_contain_blog_piece_when_article_type_for_post_is_blog_posting(): void {
+		$post_id = self::factory()->post->create(
+			array(
+				'title'        => 'indexable setting',
+				'post_content' => 'Hello world!',
+			)
+		);
+
+		\YoastSEO()->helpers->meta->set_value( 'schema_article_type', 'BlogPosting', $post_id );
+
+		// Update object to persist meta value to indexable.
+		self::factory()->post->update_object( $post_id, [] );
+
+		$this->go_to( \get_permalink( $post_id ) );
+
+		$schema_output = $this->get_schema_output();
+
+		$this->assertJson( $schema_output );
+
+		$schema_data = \json_decode( $schema_output, JSON_OBJECT_AS_ARRAY );
+
+		$article_piece = $this->get_piece_by_type( $schema_data['@graph'], [ 'Article', 'BlogPosting' ] );
+		$blog_piece = $this->get_piece_by_type( $schema_data['@graph'], 'Blog' );
+
+		$this->assertSame(['Article','BlogPosting'], $article_piece['@type'],'Article piece should be BlogPosting');
+		$this->assertSame('Blog', $blog_piece['@type'],'Blog graph piece should exit');
+		$this->assertSame($article_piece['@id'], $blog_piece['blogPost'][0]['@id'],'Blog should refer to BlogPosting');
+	}
+
+	/**
+	 * @testdox Should not contain Blog piece when the post has multiple categories
+	 */
+	public function test_should_not_contain_blog_piece_when_post_has_multiple_categories(): void {
+		$post_id = self::factory()->post->create(
+			array(
+				'title'        => 'indexable setting',
+				'post_content' => 'Hello world!',
+			)
+		);
+
+		\YoastSEO()->helpers->meta->set_value( 'schema_article_type', 'BlogPosting', $post_id );
+
+		\wp_set_post_categories(
+			$post_id,
+			[
+				self::factory()->category->create(),
+				self::factory()->category->create(),
+				self::factory()->category->create(),
+			]
+		);
+
+		// Update object to persist meta value to indexable.
+		self::factory()->post->update_object( $post_id, [] );
+
+		$this->go_to( \get_permalink( $post_id ) );
+
+		$schema_output = $this->get_schema_output();
+
+		$this->assertJson( $schema_output );
+
+		$schema_data = \json_decode( $schema_output, JSON_OBJECT_AS_ARRAY );
+
+		$article_piece = $this->get_piece_by_type( $schema_data['@graph'], [ 'Article', 'BlogPosting' ] );
+
+		$this->assertSame([ 'Article', 'BlogPosting' ], $article_piece['@type'],'Article graph piece should be BlogPosting');
+	}
+
+	private function get_schema_output( bool $debug_wpseo_head = false ): string {
+
+		ob_start();
+		do_action( 'wpseo_head' );
+		$wpseo_head = ob_get_contents();
+		ob_end_clean();
+
+		if ( $debug_wpseo_head ) {
+			print $wpseo_head;
+		}
+
+		$dom = new \DOMDocument();
+		$dom->loadHTML( $wpseo_head );
+		$scripts = $dom->getElementsByTagName('script');
+		foreach( $scripts as $script ) {
+			if( $script instanceof \DOMElement && $script->getAttribute('type') === 'application/ld+json') {
+				return $script->textContent;
+			}
+		}
+
+		throw new \LengthException('No schema script was found in the wpseo_head output.' );
+	}
+
+	/**
+	 * Find a Schema.org piece in the root of the Graph by its type.
+	 *
+	 * @param array<int, array{"@type": string}> $graph Schema.org graph.
+	 * @param string|array<int, string> $type Schema type to search for.
+	 * @return array{"@type": string} The matching schema.org piece.
+	 */
+	private function get_piece_by_type( $graph, $type ): array {
+		$nodes_of_type = array_filter( $graph, fn( $piece ) => $piece['@type'] === $type );
+
+		if ( empty( $nodes_of_type ) ) {
+			throw new \InvalidArgumentException( 'No piece found for type' );
+		}
+
+		// Return first instance.
+		return reset( $nodes_of_type );
+	}
+}
