777216b1f925686feab701447665094a42610e16
Dennis Claassen <declaassen@gmail.com>
2026-04-21 09:23:33 +0200

    Update Blog_Schema_Test.php

--- tests/Blog_Schema_Test.php
+++ tests/Blog_Schema_Test.php
@@ -120,6 +120,12 @@ class Blog_Schema_Test extends \WP_UnitTestCase {
 		$this->assertSame(['Article','BlogPosting'], $article_piece['@type'],'Article graph piece should be BlogPosting');
 		$this->assertSame('Blog', $blog_piece['@type'],'Blog graph piece should exist');
 		$this->assertSame($article_piece['@id'], $blog_piece['blogPost'][0]['@id'],'Blog should refer to BlogPosting');
+		
+		$this->assertSame(
+			'http://localhost:8889/#/schema/blog/2',
+			$blog_piece['@id'],
+			'Blog @id format {{website}}/#/schema/{{type}}/{{ID}}'
+		);
 	}
 
 	/**
