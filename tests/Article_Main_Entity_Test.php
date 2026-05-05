0f908cc377f2cc9da554f4a10021d996321ca44e
Dennis Claassen <declaassen@gmail.com>
2026-05-05 13:22:18 +0200

    Update Article_Main_Entity_Test.php

--- tests/Article_Main_Entity_Test.php
+++ tests/Article_Main_Entity_Test.php
@@ -20,6 +20,17 @@ final class Article_Main_Entity_Test extends WP_UnitTestCase {
 
 	public function set_up(): void {
 		parent::set_up();
+
+		// Create test user for publisher. Needed for Article ouput from wordpress-seo below 26.7.
+		$this->user_id = self::factory()->user->create( [
+			'display_name' => 'Test User',
+			'user_email'   => 'test@example.com',
+			'user_url'     => 'https://example.com',
+		] );
+
+		// Set Yoast user settings to use person schema
+		\YoastSEO()->helpers->options->set( 'company_or_person', 'person' );
+		\YoastSEO()->helpers->options->set( 'company_or_person_user_id', $this->user_id );
 	}
 
 	/**
