8005b70505a4d9c6cf488eb2c2da03a6a8c710b7
Dennis Claassen <declaassen@gmail.com>
2026-06-19 23:05:58 +0200

    Update Core_Code_As_Article_Part_Test.php

--- tests/Core_Code_As_Article_Part_Test.php
+++ tests/Core_Code_As_Article_Part_Test.php
@@ -1,6 +1,8 @@
 <?php
 
-final class Core_Code_As_Article_Part_Test extends \WP_TestCase {
+namespace DC23\ExcessiveSchema\Tests;
+
+final class Core_Code_As_Article_Part_Test extends \WP_UnitTestCase {
 
     public function set_up(): void {
         parent::set_up();
