f2841379a79a025ac291711dabf2ebae59af2aec
Dennis Claassen <declaassen@gmail.com>
2026-06-19 23:04:29 +0200

    Create Core_Code_As_Article_Part_Test.php

---
+++ tests/Core_Code_As_Article_Part_Test.php
@@ -0,0 +1,12 @@
+<?php
+
+final class Core_Code_As_Article_Part_Test extends \WP_TestCase {
+
+    public function set_up(): void {
+        parent::set_up();
+    }
+    
+    public function expectDeprecation() {
+        // noop.
+    }
+}
