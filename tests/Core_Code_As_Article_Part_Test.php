af03c23ce81f7017043ecb4d080d32c537925f62
Dennis Claassen <declaassen@gmail.com>
2026-06-19 23:07:58 +0200

    Update Core_Code_As_Article_Part_Test.php

--- tests/Core_Code_As_Article_Part_Test.php
+++ tests/Core_Code_As_Article_Part_Test.php
@@ -4,8 +4,8 @@ namespace DC23\ExcessiveSchema\Tests;
 
 final class Core_Code_As_Article_Part_Test extends \WP_UnitTestCase {
 
-    public function set_up(): void {
-        parent::set_up();
+	public function set_up(): void {
+		parent::set_up();
 
 		// Create test user for publisher. Needed for Article output from wordpress-seo below 26.7.
 		$this->user_id = self::factory()->user->create( [
@@ -17,10 +17,12 @@ final class Core_Code_As_Article_Part_Test extends \WP_UnitTestCase {
 		// Set Yoast user settings to use person schema
 		\YoastSEO()->helpers->options->set( 'company_or_person', 'person' );
 		\YoastSEO()->helpers->options->set( 'company_or_person_user_id', $this->user_id );
+	}
 
-    }
-    
-    public function expectDeprecation() {
-        // noop.
-    }
+	/**
+	 * Override WordPress function that's incompatible with PHPUnit 10+.
+	 */
+	public function expectDeprecated(): void {
+		// noop.
+	}
 }
