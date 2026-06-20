4668310cc34ae1f8a93d0d792dc2eaf9dc5f391b
Dennis Claassen <declaassen@gmail.com>
2026-06-20 09:12:15 +0200

    Update Core_Code_As_Article_Part.php

--- lib/Integrations/Core_Code_As_Article_Part.php
+++ lib/Integrations/Core_Code_As_Article_Part.php
@@ -6,10 +6,10 @@ final class Core_Code_As_Article_Part {
 
     public function register(): void {
         add_filter( 'wpseo_schema_block_core/code', [ $this, 'render_sourcecode_schema' ], 10, 3 );
-        
+
         add_action( 'wpseo_pre_schema_block_type_core/code', [ $this, 'prepare_sourcecode_references' ], 10, 1 );
     }
-    
+
     public function render_sourcecode_schema( $graph, $code_block, $context ) {
         array_push(
             $graph,
@@ -20,7 +20,7 @@ final class Core_Code_As_Article_Part {
                 'text' => $code_block,
             ],
         );
-        
+
         return $graph;
     }
     
@@ -30,22 +30,21 @@ final class Core_Code_As_Article_Part {
      * @param array $blocks The blocks of this type on the current page.
      */
     public function prepare_sourcecode_references( $blocks ) {
-        add_filter( 'wpseo_schema_webpage', function( $webpage_data, $context ) use ( $blocks ) {
+        add_filter( 'wpseo_schema_article', function( $article_data, $context ) use ( $blocks ) {
             $references = [];
             foreach ( $blocks as $code_block ) {
                 $references[] = [
                     '@id' => $context->canonical . '#/schema/sourcecode/',
                 ];
             }
-            
 
-                $webpage_data['hasPart'] = $webpage_data['hasPart'] ?? [];
-                array_push(
-                    $webpage_data['hasPart'],
-                    ...$references,
-                );
-            
-            return $webpage_data;
+            $article_data['hasPart'] = $article_data['hasPart'] ?? [];
+            array_push(
+                $article_data['hasPart'],
+                ...$references,
+            );
+
+            return $article_data;
         }, 10, 2 );
     }
 }
