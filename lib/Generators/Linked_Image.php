449c9f80671b23a7d02004b9f2696dbba699b4b1
Dennis Claassen <declaassen@gmail.com>
2026-06-28 20:12:15 +0200

    Create Linked_Image.php

---
+++ lib/Generators/Linked_Image.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DC23\ExcessiveSchema\Generators;
+
+use Yoast\WP\SEO\Generators\Abstract_Schema_Piece;
+
+class Linked_Image extends Abstract_Schema_Piece {
+
+    public function is_needed() {
+        return $this->context->has_article();
+    }
+    
+    public function generate() {
+        $image_pieces = [];
+        
+        return $image_pieces;
+    }
+}
