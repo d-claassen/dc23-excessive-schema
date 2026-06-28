6b0a11ca10eb4ed3e6de6e9a7419a6da32858be6
Dennis Claassen <declaassen@gmail.com>
2026-06-28 18:53:01 +0200

    Update Article_Images_Test.php

--- tests/Article_Images_Test.php
+++ tests/Article_Images_Test.php
@@ -107,6 +107,7 @@ final class Article_Images_Test extends WP_UnitTestCase {
 		
 		$this->assertArrayHasKey( $primary_image, $keyed_graph );
 		$this->assertArrayNotHasKey( $image_1_url, $keyed_graph );
+		$this->assertSame( $image_1_url, $keyed_graph[$primary_image]['url'], '1st image in graph as primary' );
 		$this->assertArrayHasKey( $image_2_url, $keyed_graph );
 	}
 
