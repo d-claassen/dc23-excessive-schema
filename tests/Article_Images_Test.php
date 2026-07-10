acba7057ef9409d88be9f5537376ac2ac013c467
Dennis Claassen <declaassen@gmail.com>
2026-07-10 10:37:38 +0200

    Update Article_Images_Test.php

--- tests/Article_Images_Test.php
+++ tests/Article_Images_Test.php
@@ -194,6 +194,9 @@ final class Article_Images_Test extends WP_UnitTestCase {
 		$keyed_graph = array_column( $schema['@graph'], null, '@id' );
 		
 		$this->assertArrayHasKey( $content_image_url, $keyed_graph );
+		
+		$this->assertSame( ['url'=>$content_inage_url], $keyed_graph[$content_image_url], 'details: 1st image in text' );
+
 		$this->assertSame( $content_image_url, $keyed_graph[$content_image_url]['url'], '1st image in text' );
 		$this->assertSame( $content_image_caption, $keyed_graph[$content_image_url]['caption'], 'block caption in schema' );
 	}
