96a7c5daedb257820f89b9734741e5e0b689bac6
Dennis Claassen <declaassen@gmail.com>
2026-07-13 06:50:36 +0200

    Update All_Article_Images.php

--- lib/Integrations/All_Article_Images.php
+++ lib/Integrations/All_Article_Images.php
@@ -34,32 +34,10 @@ class All_Article_Images {
 		if ( ! $context->indexable ) {
 			return $article;
 		}
-
-        $images = array_filter(
-            $this->get_links_repo()->find_all_by_indexable_id( $context->indexable->id ),
-			fn( $link ) => in_array( $link->type, [ SEO_Links::TYPE_INTERNAL_IMAGE, SEO_Links::TYPE_EXTERNAL_IMAGE ], true ),
-		);
-
-		if ( empty( $images ) ) {
-			return $article;
-		}
         
-        if ( ! empty( $article['image'] ) && ! array_is_list( $article['image'] ) ) {
-            // Wrap one associative array within a new array list.
-            $article['image'] = [ $article['image'] ];
-        }
+        $image_ids = [];
+        $image_counts = [];
 
-        foreach ( $images as $image ) {
-            // @TODO. Consider cases where $image->post_target_id needs comparing with $context->main_image_id
-            if ( $image->url === $context->main_image_url ) {
-                continue;
-            }
-
-            $article['image'][] = [
-                '@id' => $image->url,
-            ];
-        }
-        
         foreach ( $context->blocks['core/image'] as $block ) {
             $processor = new \WP_HTML_Tag_Processor( $block['innerHTML'] );
 
@@ -73,12 +51,49 @@ class All_Article_Images {
             }
             
             if ( $block_src ) {
-                $article['image'][] = [
-                    '@id' => $context->canonical . '#/schema/ImageObject/' . md5( $block_src ),
+                $image_counts[$block_src] ??= 0;
+                ++$image_counts[$block_src];
+                
+                $image_ids[] = [
+                    '@id' => $context->canonical . '#/schema/ImageObject/' . md5( $block_src ) . '-' . $image_counts[$block_src],
                 ];
             }
         }
-        $article['image'][] = $context->blocks['core/image'];
+
+
+        $images = array_filter(
+            $this->get_links_repo()->find_all_by_indexable_id( $context->indexable->id ),
+			fn( $link ) => in_array( $link->type, [ SEO_Links::TYPE_INTERNAL_IMAGE, SEO_Links::TYPE_EXTERNAL_IMAGE ], true ),
+		);
+
+        foreach ( $images as $image ) {
+            // @TODO. Consider cases where $image->post_target_id needs comparing with $context->main_image_id?
+            if ( $image->url === $context->main_image_url ) {
+                continue;
+            }
+
+            if ( ! array_key_exists( $image->url, $image_counts ) ) {
+                continue;
+            }
+
+            $image_counts[$image->url] = 1;
+            $image_ids[] = [
+                '@id' => $context->canonical . '#/schema/ImageObject/' . md5( $image->url ) . '-' . $image_counts[$image->url],
+            ];
+        }
+        
+        if ( empty( $image_ids ) ) {
+            return $article;
+        }
+        
+        if ( ! empty( $article['image'] ) && ! array_is_list( $article['image'] ) ) {
+            // Wrap one associative array within a new array list.
+            $article['image'] = [ $article['image'] ];
+        }
+
+        array_push( $article['image'], ...$image_ids );
+
+        // $article['image'][] = $context->blocks['core/image'];
 
         return $article;
     }
