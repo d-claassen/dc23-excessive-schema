7a3b0b2972f0f83fbffa122121b850b63a2c41fe
Dennis Claassen <declaassen@gmail.com>
2026-07-15 21:05:55 +0200

    Update Linked_Image.php

--- lib/Generators/Linked_Image.php
+++ lib/Generators/Linked_Image.php
@@ -32,8 +32,10 @@ class Linked_Image extends Abstract_Schema_Piece {
                         $block_caption .= '/'.$processor->get_tag();
                         break;
                     case 'FIGCAPTION':
-                        $processor->next_token();
-                        $block_caption .= var_export( trim( $processor->get_modifiable_text() ), true );
+                        $processor->set_bookmark('figcaption');
+                        while( $processor->next_token() && ! ( $processor->get_tag() === 'FIGCAPTION' && $processor->is_tag_closer() ) {
+                            $block_caption .= var_export( trim( $processor->get_modifiable_text() ), true );
+                        }
                     default:
                         $block_caption .= '/'.$processor->get_tag();
                         break;
